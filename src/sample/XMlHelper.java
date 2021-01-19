51b209e9bbee853f969d99253b20ef0f0cc4f07c
DeMon <757454343@qq.com>
2021-01-19 18:13:11 +0800

    fix

--- src/sample/XMlHelper.java
+++ src/sample/XMlHelper.java
@@ -2,10 +2,8 @@ package sample;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
-
 import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
